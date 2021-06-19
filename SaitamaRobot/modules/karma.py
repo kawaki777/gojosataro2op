aea462538e2c68d14378f459293de558ef5441be
kawaki <84354777+kawaki777@users.noreply.github.com>
2021-06-19 16:37:51 +0530

    Fixed karma

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -10,7 +10,7 @@ regex_upvote = r"^((?i)\+|\+\+|\+1|thx|tnx|ty|thank you|thanx|thanks|pro|cool|go
 regex_downvote = r"^(\-|\-\-|\-1|👎|noob|Noob|gross|fuck off)$"
 
 
-@EREN.on_message(
+@GOJO.on_message(
     filters.text
     & filters.group
     & filters.incoming
@@ -43,7 +43,7 @@ async def upvote(_, message):
     )
 
 
-@EREN.on_message(
+@GOJO.on_message(
     filters.text
     & filters.group
     & filters.incoming
@@ -76,7 +76,7 @@ async def downvote(_, message):
     )
 
 
-@EREN.on_message(filters.command("karma") & filters.group)
+@GOJO.on_message(filters.command("karma") & filters.group)
 @capture_err
 async def karma(_, message):
     chat_id = message.chat.id
